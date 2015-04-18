4d7daaad68c1ac01a63b9a8587d465a643e391eb
Jordan Fisher <jordan.efisher@gmail.com>
2015-04-18 12:54:31 -0400

    Added ui skeleton for game lobby kingdom/team choice.

--- Game/html/js/lib/Components/GameLobby.js
+++ Game/html/js/lib/Components/GameLobby.js
@@ -1,7 +1,11 @@
 define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Components/Chat'], function(_, React, ReactBootstrap, interop, events, ui, Chat) {
     var Panel = ReactBootstrap.Panel;
     var Button = ReactBootstrap.Button;
+    var Well = ReactBootstrap.Well;
     var Popover = ReactBootstrap.Popover;
+    var DropdownButton = ReactBootstrap.DropdownButton;
+    var MenuItem = ReactBootstrap.MenuItem;
+    var Table = ReactBootstrap.Table;
     
     var Div = ui.Div;
     var Gap = ui.Gap;
@@ -14,6 +18,70 @@ define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Compon
     var width = ui.width;
     var subImage = ui.subImage;
 
+    var ChooseKingdom = React.createClass({displayName: "ChooseKingdom",
+        mixins: [],
+                
+        getInitialState: function() {
+            return {
+            };
+        },
+        
+        render: function() {
+            return (
+                React.createElement("div", {style: {'pointer-events':'auto'}}, 
+                    React.createElement(DropdownButton, {title: "Choose your Kingdom"}, 
+                        React.createElement(MenuItem, null, "Kingdom of Wei"), 
+                        React.createElement(MenuItem, null, "Kingdom of Shu"), 
+                        React.createElement(MenuItem, null, "Kingdom of Shen"), 
+                        React.createElement(MenuItem, null, "Kingdom of Beast")
+                    )
+                )
+            );
+        },
+    });
+
+    var ChooseTeam = React.createClass({displayName: "ChooseTeam",
+        mixins: [],
+                
+        getInitialState: function() {
+            return {
+            };
+        },
+        
+        render: function() {
+            return (
+                React.createElement("div", {style: {'pointer-events':'auto'}}, 
+                    React.createElement(DropdownButton, {title: "Team 1"}, 
+                        _.map(_.range(1, 5), function(i) { return (
+                            React.createElement(MenuItem, null, 
+                                "Team ", i
+                            )
+                        );})
+                    )
+                )
+            );
+        },
+    });
+
+    var PlayerEntry = React.createClass({displayName: "PlayerEntry",
+        mixins: [],
+                
+        getInitialState: function() {
+            return {
+            };
+        },
+        
+        render: function() {
+            return (
+                React.createElement("tr", null, 
+                    React.createElement("td", null, "Player ", this.props.player), 
+                    React.createElement("td", null, React.createElement(ChooseTeam, null)), 
+                    React.createElement("td", null, React.createElement(ChooseKingdom, null))
+                )
+            );
+        },
+    });
+
     return React.createClass({
         mixins: [],
                 
@@ -25,22 +93,30 @@ define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Compon
         render: function() {
             return (
                 React.createElement("div", null, 
-                    React.createElement(Div, {nonBlocking: true, pos: pos(10,10), size: width(60)}, 
+                    React.createElement(Div, {nonBlocking: true, pos: pos(10,5), size: width(80)}, 
                         React.createElement(Panel, null, 
-                            React.createElement("p", null, 
+                            React.createElement("h2", null, 
                                 "Game Lobby"
                             )
                         ), 
 
-                        React.createElement(Chat.ChatBox, {show: true, pos: pos(.38, 78), size: width(38)}), 
-                        React.createElement(Chat.ChatInput, {show: true, pos: pos(.35,80), size: width(49)}), 
+                        React.createElement(Well, {style: {'height':'75%'}}, 
+                            React.createElement(Chat.ChatBox, {show: true, pos: pos(2, 78), size: width(43)}), 
+                            React.createElement(Chat.ChatInput, {show: true, pos: pos(2,80), size: width(43)}), 
 
-                        React.createElement(Div, {nonBlocking: true, pos: pos(40,80), size: width(60)}, 
-                            React.createElement("div", {style: {'float':'right', 'pointer-events':'auto'}}, 
-                                React.createElement("p", null, 
-                                    React.createElement(Button, null, "Start Game"), 
-                                    " ", 
-                                    React.createElement(Button, null, "Leave Lobby")
+                            React.createElement(Div, {nonBlocking: true, pos: pos(48,20), size: width(50), style: {'pointer-events':'auto', 'font-size': '1.4%;'}}, 
+                                React.createElement(Table, {style: {width:'100%'}}, React.createElement("tbody", null, 
+                                    _.map(_.range(1, 5), function(i) { return React.createElement(PlayerEntry, {player: i}); })
+                                ))
+                            ), 
+
+                            React.createElement(Div, {nonBlocking: true, pos: pos(38,80), size: width(60)}, 
+                                React.createElement("div", {style: {'float':'right', 'pointer-events':'auto'}}, 
+                                    React.createElement("p", null, 
+                                        React.createElement(Button, null, "Start Game"), 
+                                        " ", 
+                                        React.createElement(Button, null, "Leave Lobby")
+                                    )
                                 )
                             )
                         )
@@ -49,4 +125,4 @@ define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Compon
             );
         }
     });
-});
+}); 
--- Game/html/jsx/lib/Components/GameLobby.js
+++ Game/html/jsx/lib/Components/GameLobby.js
@@ -1,7 +1,11 @@
 define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Components/Chat'], function(_, React, ReactBootstrap, interop, events, ui, Chat) {
     var Panel = ReactBootstrap.Panel;
     var Button = ReactBootstrap.Button;
+    var Well = ReactBootstrap.Well;
     var Popover = ReactBootstrap.Popover;
+    var DropdownButton = ReactBootstrap.DropdownButton;
+    var MenuItem = ReactBootstrap.MenuItem;
+    var Table = ReactBootstrap.Table;
     
     var Div = ui.Div;
     var Gap = ui.Gap;
@@ -14,6 +18,70 @@ define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Compon
     var width = ui.width;
     var subImage = ui.subImage;
 
+    var ChooseKingdom = React.createClass({
+        mixins: [],
+                
+        getInitialState: function() {
+            return {
+            };
+        },
+        
+        render: function() {
+            return (
+                <div style={{'pointer-events':'auto'}}>
+                    <DropdownButton title='Choose your Kingdom'>
+                        <MenuItem>Kingdom of Wei</MenuItem>
+                        <MenuItem>Kingdom of Shu</MenuItem>
+                        <MenuItem>Kingdom of Shen</MenuItem>
+                        <MenuItem>Kingdom of Beast</MenuItem>
+                    </DropdownButton>
+                </div>
+            );
+        },
+    });
+
+    var ChooseTeam = React.createClass({
+        mixins: [],
+                
+        getInitialState: function() {
+            return {
+            };
+        },
+        
+        render: function() {
+            return (
+                <div style={{'pointer-events':'auto'}}>
+                    <DropdownButton title='Team 1'>
+                        {_.map(_.range(1, 5), function(i) { return (
+                            <MenuItem>
+                                Team {i}
+                            </MenuItem>
+                        );})}
+                    </DropdownButton>
+                </div>
+            );
+        },
+    });
+
+    var PlayerEntry = React.createClass({
+        mixins: [],
+                
+        getInitialState: function() {
+            return {
+            };
+        },
+        
+        render: function() {
+            return (
+                <tr>
+                    <td>Player {this.props.player}</td>
+                    <td><ChooseTeam /></td>
+                    <td><ChooseKingdom /></td>
+                </tr>
+            );
+        },
+    });
+
     return React.createClass({
         mixins: [],
                 
@@ -25,28 +93,36 @@ define(['lodash', 'react', 'react-bootstrap', 'interop', 'events', 'ui', 'Compon
         render: function() {
             return (
                 <div>
-                    <Div nonBlocking pos={pos(10,10)} size={width(60)}>
+                    <Div nonBlocking pos={pos(10,5)} size={width(80)}>
                         <Panel>
-                            <p>
+                            <h2>
                                 Game Lobby
-                            </p>
+                            </h2>
                         </Panel>
 
-                        <Chat.ChatBox show pos={pos(.38, 78)} size={width(38)}/>
-                        <Chat.ChatInput show pos={pos(.35,80)} size={width(49)} />
+                        <Well style={{'height':'75%'}}>
+                            <Chat.ChatBox show pos={pos(2, 78)} size={width(43)}/>
+                            <Chat.ChatInput show pos={pos(2,80)} size={width(43)} />
 
-                        <Div nonBlocking pos={pos(40,80)} size={width(60)}>
-                            <div style={{'float':'right', 'pointer-events':'auto'}}>
-                                <p>
-                                    <Button>Start Game</Button>
-                                    &nbsp;
-                                    <Button>Leave Lobby</Button>
-                                </p>
-                            </div>
-                        </Div>
+                            <Div nonBlocking pos={pos(48,20)} size={width(50)} style={{'pointer-events':'auto', 'font-size': '1.4%;'}}>
+                                <Table style={{width:'100%'}}><tbody>
+                                    {_.map(_.range(1, 5), function(i) { return <PlayerEntry player={i} />; })}
+                                </tbody></Table>
+                            </Div>
+
+                            <Div nonBlocking pos={pos(38,80)} size={width(60)}>
+                                <div style={{'float':'right', 'pointer-events':'auto'}}>
+                                    <p>
+                                        <Button>Start Game</Button>
+                                        &nbsp;
+                                        <Button>Leave Lobby</Button>
+                                    </p>
+                                </div>
+                            </Div>
+                        </Well>
                     </Div>
                 </div>
             );
         }
     });
-});
+}); 
