a4491f21fcd216dac01ff656b1f4d7471347f969
Jordan Fisher <Jordan Fisher>
2015-12-17 21:22:25 -0500

    Removed System.Web.Extensions.

--- Game/Ui/UiCore.cs
+++ Game/Ui/UiCore.cs
@@ -1,37 +1,21 @@
 using System;
 using System.IO;
 
-using Windows = System.Windows.Forms;
-
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-using FragSharpHelper;
-using FragSharpFramework;
 using Input = FragSharpHelper.Input;
 
 using Awesomium.Core;
 using Awesomium.Core.Data;
-using Awesomium.Core.Dynamic;
 using AwesomiumXNA;
-
-using System.Text;
-using System.Net;
-using System.Net.Sockets;
 using System.Threading;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Web.Script.Serialization;
 
 using Newtonsoft.Json;
 
 namespace Game
 {
-    using Dict = Dictionary<string, object>;
-
     public class MainDataSource : DataSource
     {
         protected override void OnRequest(DataSourceRequest request)
